bb9ae68c467d4a50c94e520b61674687bdf4a547
108charlotte <152224795+108charlotte@users.noreply.github.com>
2025-05-30 10:17:11 +0000

    super simple fastAPI setup from youtube tutorial (see resources.md for link)

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+from fastapi import FastAPI
+from app.router import router as process_router
+
+app = FastAPI()
+app.include_router(process_router)
+
+@app.get("/")
+def root():
+    return {"message": "Hello, world!"}
---
+++ resources.md
@@ -0,0 +1,4 @@
+FastAPI: 
+https://fastapi.tiangolo.com/tutorial/
+https://fastapi.tiangolo.com/tutorial/first-steps/
+https://www.youtube.com/watch?v=-IaCV5-mlSk
